88795b99a082427dc66848e9c8362a6978accf19
wendy-160 <wendy.lacan002@gmail.com>
2025-03-14 22:06:16 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,6 @@ function App() {
       <Navbar/>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login/>} />
       </Routes>
     </Router>  
 );
