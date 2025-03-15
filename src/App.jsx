d685bbfec91a203617e3ad82027036ac6d7521e4
wendy-160 <wendy.lacan002@gmail.com>
2025-03-14 21:40:07 -0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from 'react'
+import Navbar from "./components/Home/Navbar";
+import Home from "./components/Home/Home";
+//import Login from "./components/Login"
+
+function App() {
+  return(
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login/>} />
+      </Routes>
+    </Router>  
+);
+  
+}
+
+export default App
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,14 @@
+import "./Home.css";
+import Footer from "./Footer";
+
+const Home = () => {
+    return (
+      <div className="home">
+        <h1>UH ClinicHub</h1>
+        <p>Wherever the art of medicine is loved, there is also a love of humanity</p>
+        <Footer />
+      </div>
+    );
+  };
+  
+  export default Home;
